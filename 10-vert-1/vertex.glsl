142efefc6c1e41cd29056eb90cc3eb9f6ac2d40f
krzychukula <krzychukula@gmail.com>
2016-05-05 17:43:17 +0200

    This one was WAY to hard for me :(

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -8,5 +8,15 @@ void main() {
 
   //TODO: rotate position by theta radians about the origin
 
-  gl_Position = vec4(position, 0, 1.0);
+  //ok I had to copy-paste this one from soulutions
+  float c = cos(theta);
+  float s = sin(theta);
+  //this matrix is really magical for me, how am I supposed to know
+  //that I have to create rotation matrix and even then
+  //how am I supposed to figure out the order of cos and sin not to mention
+  //the signs...
+  mat2 rot = mat2(c, s, -s, c);
+
+  gl_Position = vec4(rot * position, 0, 1.0);
+
 }
