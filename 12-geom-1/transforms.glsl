301dcf3ce937ab9d4072cf8c869ffc067b1050e1
krzychukula <krzychukula@gmail.com>
2016-05-06 07:51:42 +0200

    clip coordinates

--- 12-geom-1/transforms.glsl
+++ 12-geom-1/transforms.glsl
@@ -7,6 +7,9 @@ uniform mat4 model, view, projection;
 void main() {
 
   //TODO: Apply the model-view-projection matrix to `position`
+  vec4 inTheWolrd = model * vec4(position, 1.0);
+  vec4 inTheCameraView = view * inTheWolrd;
+  vec4 InTheDeviceClipCoordinates = projection * inTheCameraView;
 
-  gl_Position = vec4(position, 1);
+  gl_Position = InTheDeviceClipCoordinates;
 }
