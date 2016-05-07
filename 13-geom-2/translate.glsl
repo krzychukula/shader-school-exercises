de49216da10082348bfae81afe96ff5a1215c137
krzychukula <krzychukula@gmail.com>
2016-05-07 07:44:03 +0200

    Translation matrix

--- 13-geom-2/translate.glsl
+++ 13-geom-2/translate.glsl
@@ -1,12 +1,13 @@
 highp mat4 translate(highp vec3 p) {
 
-  //TODO: Construct a matrix, m, which translates all points so that p is at the origin.
-
+  //TODO: Construct a matrix, m,
+  // which translates all points so that p is at the origin.
+  
+  //https://en.wikipedia.org/wiki/Translation_(geometry)
   return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
+              0, 1, 0, 0,
               0, 0, 1, 0,
-              0, 0, 0, 1);
+              -p.x, -p.y, -p.z, 1);
 }
-
 //Do not remove this line
 #pragma glslify: export(translate)
