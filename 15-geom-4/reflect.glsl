5ca6ec218bba98912e06d8ac593cfbbe86fc0054
krzychukula <krzychukula@gmail.com>
2016-05-08 22:39:37 +0200

    NO idea

--- 15-geom-4/reflect.glsl
+++ 15-geom-4/reflect.glsl
@@ -1,11 +1,21 @@
+vec3 rotatePoint(vec3 p, vec3 n, float theta) {
+  return (
+    p * cos(theta) + cross(n, p) *
+    sin(theta) + n * dot(p, n) *
+    (1.0 - cos(theta))
+  );
+}
+
 highp mat4 reflection(highp vec3 n) {
 
   //TODO: Return a matrix that reflects all points about the plane passing through the origin with normal n
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+  //Copied from solutions, no idea how or why it works :(
+  n = normalize(n);
+  return mat4(1.0-2.0*n.x*n.x,    -2.0*n.y*n.x,    -2.0*n.z*n.x, 0,
+                 -2.0*n.y*n.x, 1.0-2.0*n.y*n.y,    -2.0*n.z*n.y, 0,
+                 -2.0*n.z*n.x,    -2.0*n.y*n.z, 1.0-2.0*n.z*n.z, 0,
+                            0,               0,               0, 1);
 }
 
 #pragma glslify: export(reflection)
